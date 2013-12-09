c3427d0f48f4205d320334dba9b8067b9057874a
kalrover <ktieudao@gmail.com>
2013-12-10 01:15:33 +0900

    Finished header / The bloginfo() Function

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html lang='en'> 
+	<head> 
+	<title><?php bloginfo('name'); wp_title(); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/reset.css" type="text/css"> 
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css"> 
+	<!--[if IE]>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/ie.css" type="text/css" />
+	<![endif]-->
+	</head> 
+	<body class="home blog">
+	<div id="top-bar-tile">
+		<div id="top-bar-content">
+			<h1><a href="http://www.danwalker.com/themes/blindfaith"><?php bloginfo('name'); ?></a></h1>
+			<span class="slogan"><?php bloginfo('description'); ?></span>
+			<div id="search-box">
+				<form method="get" id="searchform" action="" > 
+					<input type="text" value="Search..." onfocus="if(this.value == this.defaultValue) this.value = ''" name="s" id="s" /> 
+				</form>
+			</div><!-- search-box -->
+		</div><!-- top-bar-content -->
+	</div><!-- top-bar-tile -->
+	<div id="nav-bar-tile">
+			 <div class="nav-bar-content"><ul><li class="current_page_item"><a href="http://www.danwalker.com/themes/blindfaith/" title="Home">Home</a></li><li class="page_item page-item-2"><a href="http://www.danwalker.com/themes/blindfaith/?page_id=2" title="About">About</a></li><li class="page_item page-item-4"><a href="http://www.danwalker.com/themes/blindfaith/?page_id=4" title="Typography">Typography</a></li></ul></div>
+	</div><!-- nav-bar-tile -->
+	<div id="wrapper">
+		<div id="content">
